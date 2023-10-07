ecf80c1fc95f7beb584852b9cd394005bbfe0738
StanDobrev11 <dobrev81@gmail.com>
2023-10-07 16:33:09 +0000

    update

--- 02_First_Steps_in_OOP_-_Exercise/05_flower.py
+++ 02_First_Steps_in_OOP_-_Exercise/05_flower.py
@@ -12,10 +12,10 @@ Submit only the class in the judge system.
 
 
 class Flower:
-    def __init__(self, name: str, water_requirements: int, is_happy=False):
+    def __init__(self, name: str, water_requirements: int):
         self.name = name
         self.water_requirements = water_requirements
-        self.is_happy = is_happy
+        self.is_happy = False
 
     def water(self, quantity) -> None:
         if quantity >= self.water_requirements:
--- 02_First_Steps_in_OOP_-_Exercise/08_Pokemon_Battle/trainer.py
+++ 02_First_Steps_in_OOP_-_Exercise/08_Pokemon_Battle/trainer.py
@@ -1,21 +1,41 @@
+from pokemon import Pokemon
+from typing import List
+
+
 class Trainer:
     def __init__(self, name: str):
         self.name = name
-        self.pokemons = []
+        self.pokemons: List[Pokemon] = []
 
-    def add_pokemon(self, pokemon):
+    def add_pokemon(self, pokemon: Pokemon):
         if pokemon in self.pokemons:
             return "This pokemon is already caught"
         self.pokemons.append(pokemon)
         return "Caught " + pokemon.pokemon_details()
 
     def release_pokemon(self, name: str):
-        for pokemon in self.pokemons:
-            if name == pokemon.name:
-                self.pokemons.remove(pokemon)
-                return f"You have released {name}"
-        else:
+        try:
+            pokemon = next(filter(lambda p: p.name == name, self.pokemons))
+        except StopIteration:
             return "Pokemon is not caught"
+
+        self.pokemons.remove(pokemon)
+        return f"You have released {name}"
+
+        # try:
+        #     pokemon = [p for p in self.pokemons if p.name == name][0]
+        # except IndexError:
+        #     return "Pokemon is not caught"
+        #
+        # self.pokemons.remove(pokemon)
+        # return f"You have released {name}"
+
+        # for pokemon in self.pokemons:
+        #     if name == pokemon.name:
+        #         self.pokemons.remove(pokemon)
+        #         return f"You have released {name}"
+        # else:
+        #     return "Pokemon is not caught"
 
     def trainer_data(self):
         data = (f"Pokemon Trainer {self.name}\n"
