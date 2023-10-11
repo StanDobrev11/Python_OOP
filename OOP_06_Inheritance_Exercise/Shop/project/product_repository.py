d073253c8adeaf5332e38a192143026e99bda2c6
StanDobrev11 <dobrev81@gmail.com>
2023-10-11 18:37:00 +0000

    Updated repo file to avoid code repetition in remove method

--- OOP_06_Inheritance_Exercise/Shop/project/product_repository.py
+++ OOP_06_Inheritance_Exercise/Shop/project/product_repository.py
@@ -1,5 +1,4 @@
 from project.product import Product
-from typing import ClassVar
 
 
 class ProductRepository:
@@ -7,26 +6,26 @@ class ProductRepository:
     It is a repository for all products that are delivered to the grocery shop.
     """
 
-    def __init__(self):
-        self.products: list[ClassVar] = []
+    def __init__(self) -> None:
+        self.products: list[Product] = []
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         # "{product_name1}: {quantity1}\n{product_name2}: {quantity2}\n..."
         return '\n'.join(f"{product.name}: {product.quantity}" for product in self.products)
 
     def add(self, product: Product) -> None:
         self.products.append(product)
 
-    def find(self, product_name: str):
+    def find(self, product_name: str) -> Product:
         try:
             product = next(filter(lambda n: n.name == product_name, self.products))
             return product
         except StopIteration:
             pass
 
-    def remove(self, product_name: str):
+    def remove(self, product_name: str) -> None:
         try:
-            product = next(filter(lambda n: n.name == product_name, self.products))
+            product = self.find(product_name)
             self.products.remove(product)
-        except StopIteration:
+        except ValueError:
             pass
