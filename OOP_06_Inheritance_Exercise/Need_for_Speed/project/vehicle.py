34d93092ff3a75c4acb18dee6a483dc9c16a7cf0
StanDobrev11 <dobrev81@gmail.com>
2023-10-11 16:43:32 +0000

    Completed task 100/100

--- OOP_06_Inheritance_Exercise/Need_for_Speed/project/vehicle.py
+++ OOP_06_Inheritance_Exercise/Need_for_Speed/project/vehicle.py
@@ -1,12 +1,13 @@
 class Vehicle:
     DEFAULT_FUEL_CONSUMPTION: float = 1.25
 
-    def __init__(self, fuel: float, horse_power: int):
+    def __init__(self, fuel: float, horse_power: int) -> None:
         self.fuel = fuel  # quantity of fuel in a specific vehicle
         self.horse_power = horse_power
         self.fuel_consumption = self.__class__.DEFAULT_FUEL_CONSUMPTION  # fuel consumption per kilometer
 
-    def drive(self, kilometers: int):
-        self.fuel_consumption = self.__class__.DEFAULT_FUEL_CONSUMPTION * kilometers
-        self.fuel = 0 if self.fuel - self.fuel_consumption <= 0 else self.fuel - self.fuel_consumption
+    def drive(self, kilometers: int) -> None:
+        fuel_needed = kilometers * self.__class__.DEFAULT_FUEL_CONSUMPTION
 
+        if self.fuel >= fuel_needed:
+            self.fuel -= fuel_needed
