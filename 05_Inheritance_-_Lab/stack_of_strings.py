0458511ad23332c38df193527df1fa89e00e00f7
StanDobrev11 <dobrev81@gmail.com>
2023-10-11 00:03:17 +0000

    Update

---
+++ 05_Inheritance_-_Lab/stack_of_strings.py
@@ -0,0 +1,62 @@
+from typing import List
+
+
+
+class Stack:
+    def __init__(self):
+        self.data: List = []
+
+    def __str__(self) -> str:
+        return f"[{', '.join(self.data[::-1])}]"  # "[{element(N)}, {element(N-1)} ... {element(0)}]"
+
+    def push(self, element: str) -> None:
+        if isinstance(element, str):
+            self.data.append(element)
+
+    def pop(self) -> str:
+        return self.data.pop()
+
+    def top(self) -> str:
+        return self.data[-1]
+
+    def is_empty(self):
+        return False if self.data else True
+
+
+# class BaseStack:
+#     def __init__(self):
+#         self.data: List[str] = []
+#
+#     def is_empty(self):
+#         return False if self.data else True
+#
+#     def __str__(self):
+#         return f"[{', '.join(self.data[::-1])}]"
+#
+#
+# class AddStack(BaseStack):
+#     def push(self, element: str) -> None:
+#         if isinstance(element, str):
+#             self.data.append(element)
+#
+#
+# class RemoveStack(BaseStack):
+#     def pop(self) -> str:
+#         return self.data.pop()
+#
+#
+# class TopStack(BaseStack):
+#     def top(self) -> None:
+#         self.data.pop()
+#
+#
+# class Stack(AddStack, RemoveStack, TopStack):
+#     pass
+
+
+# stack = Stack()
+# print(stack)
+# stack.push('1')
+# stack.push('2')
+# stack.push('3')
+# print()
