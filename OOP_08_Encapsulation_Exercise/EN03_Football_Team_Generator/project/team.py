db4adc73846427a19dc560e1e07154605f28fdc6
StanDobrev11 <dobrev81@gmail.com>
2023-10-13 22:29:06 +0000

    Task done 100/100

---
+++ OOP_08_Encapsulation_Exercise/EN03_Football_Team_Generator/project/team.py
@@ -0,0 +1,26 @@
+from project.player import Player
+from typing import List
+
+
+class Team:
+    def __init__(self, name: str, rating: int) -> None:
+        self.__name = name
+        self.__rating = rating
+        self.__players: List[Player] = []
+
+    def add_player(self, player: Player) -> str:
+        """adds player to team"""
+        if player in self.__players:
+            return f"Player {player.name} has already joined"
+
+        self.__players.append(player)
+        return f"Player {player.name} joined team {self.__name}"
+
+    def remove_player(self, player_name: str):
+        """removes player from team"""
+        try:
+            player = [p for p in self.__players if p.name == player_name][0]
+            self.__players.remove(player)
+            return player
+        except IndexError:
+            return f"Player {player_name} not found"
