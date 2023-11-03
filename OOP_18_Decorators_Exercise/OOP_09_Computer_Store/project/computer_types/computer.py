0f58beaed7aa1b406e17ac0b6cccfd5aad99df04
StanDobrev11 <dobrev81@gmail.com>
2023-11-03 06:43:39 +0000

    update

--- OOP_18_Decorators_Exercise/OOP_09_Computer_Store/project/computer_types/computer.py
+++ OOP_18_Decorators_Exercise/OOP_09_Computer_Store/project/computer_types/computer.py
@@ -2,12 +2,35 @@ from abc import ABC, abstractmethod
 
 
 class Computer(ABC):
+    _processors_prices = {}
+    _ram_prices = {}
+
     def __init__(self, manufacturer: str, model: str):
         self.manufacturer = manufacturer
         self.model = model
-        self.__processor = None
-        self.__ram = None
-        self.price = None
+        self.processor = None
+        self.ram = None
+        self.price = 0
+
+    @property
+    @abstractmethod
+    def processor(self):
+        ...
+
+    @processor.setter
+    @abstractmethod
+    def processor(self, value):
+        ...
+
+    @property
+    @abstractmethod
+    def ram(self):
+        ...
+
+    @ram.setter
+    @abstractmethod
+    def ram(self, value):
+        ...
 
     @property
     def manufacturer(self) -> str:
@@ -15,6 +38,7 @@ class Computer(ABC):
 
     @manufacturer.setter
     def manufacturer(self, value: str):
+        # if value.strip() == "" -> better solution
         if not value or all(x == ' ' for x in value):
             raise ValueError("Manufacturer name cannot be empty.")
         self.__manufacturer = value
@@ -29,9 +53,14 @@ class Computer(ABC):
             raise ValueError("Model name cannot be empty.")
         self.__model = value
 
-    @abstractmethod
-    def configure_computer(self, processor: str, ram: int) -> None:
-        ...
+    def update_price(self):
+        self.price = self.__class__._processors_prices[self.processor] + self.__class__._ram_prices[self.ram]
+
+    def configure_computer(self, processor: str, ram: int):
+        self.processor = processor
+        self.ram = ram
+        self.update_price()
+        return f"Created {self.manufacturer} {self.model} with {self.processor} and {self.ram}GB RAM for {self.price}$."
 
     def __repr__(self) -> str:
         return f"{self.manufacturer} {self.model} with {self.processor} and {self.ram}GB RAM"
--- OOP_18_Decorators_Exercise/OOP_09_Computer_Store/project/computer_types/desktop_computer.py
+++ OOP_18_Decorators_Exercise/OOP_09_Computer_Store/project/computer_types/desktop_computer.py
@@ -13,27 +13,14 @@ class DesktopComputer(Computer):
     _ram_prices = {2 ** power_count: 100 * power_count for power_count in
                    range(1, int(math.log(_max_ram_capacity * 2, 2)))}
 
-    def update_price(self):
-        self.price = self.__class__._processors_prices[self.processor] + self.__class__._ram_prices[self.ram]
-
-    def configure_computer(self, processor: str, ram: int):
-        if processor not in self.__class__._processors_prices:
-            raise ValueError(f"{processor} is not compatible with desktop computer {self.manufacturer} {self.model}!")
-        if ram not in self.__class__._ram_prices:
-            raise ValueError(f"{ram}GB RAM is not compatible with desktop computer {self.manufacturer} {self.model}!")
-        self.__processor = processor
-        self.__ram = ram
-        self.update_price()
-        return f"Created {self.manufacturer} {self.model} with {self.processor} and {self.ram}GB RAM for {self.price}$."
-
     @property
     def processor(self):
         return self.__processor
 
     @processor.setter
     def processor(self, value):
-        if value not in self.__class__._processors_prices:
-            raise ValueError(f"{value} is not compatible with laptop {self.manufacturer} {self.model}!")
+        if value not in self.__class__._processors_prices and value is not None:
+            raise ValueError(f"{value} is not compatible with desktop computer {self.manufacturer} {self.model}!")
         self.__processor = value
 
     @property
@@ -42,6 +29,6 @@ class DesktopComputer(Computer):
 
     @ram.setter
     def ram(self, value):
-        if value not in self.__class__._ram_prices:
-            raise ValueError(f"{value}GB RAM is not compatible with laptop {self.manufacturer} {self.model}!")
+        if value not in self.__class__._ram_prices and value is not None:
+            raise ValueError(f"{value}GB RAM is not compatible with desktop computer {self.manufacturer} {self.model}!")
         self.__ram = value
