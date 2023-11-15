f988412faeb4491121ebff0eb017991737ea5119
StanDobrev11 <dobrev81@gmail.com>
2023-11-15 07:01:53 +0000

    Updated using sets and issubset() to perform checks

--- OOP_Exams/12_DEC_2022_RETAKE/TASK/project/concert_tracker_app.py
+++ OOP_Exams/12_DEC_2022_RETAKE/TASK/project/concert_tracker_app.py
@@ -11,23 +11,41 @@ from project.concert import Concert
 class ConcertTrackerApp:
     MUSICIANS = {"Guitarist": Guitarist, "Drummer": Drummer, "Singer": Singer}
 
+    # SKILL_SET = {
+    #     "Rock": {
+    #         "Drummer": ["play the drums with drumsticks"],
+    #         "Singer": ["sing high pitch notes"],
+    #         "Guitarist": ["play rock"]
+    #     },
+    #
+    #     'Metal': {
+    #         "Drummer": ["play the drums with drumsticks"],
+    #         "Singer": ["sing low pitch notes"],
+    #         "Guitarist": ["play metal"]
+    #     },
+    #     "Jazz": {
+    #         "Drummer": ["play the drums with drum brushes"],
+    #         "Singer": ["sing high pitch notes", "sing low pitch notes"],
+    #         "Guitarist": ["play jazz"]
+    #     }
+    # }
     SKILL_SET = {
-        "Rock": {
-            "Drummer": ["play the drums with drumsticks"],
-            "Singer": ["sing high pitch notes"],
-            "Guitarist": ["play rock"]
-        },
-
-        'Metal': {
-            "Drummer": ["play the drums with drumsticks"],
-            "Singer": ["sing low pitch notes"],
-            "Guitarist": ["play metal"]
-        },
-        "Jazz": {
-            "Drummer": ["play the drums with drum brushes"],
-            "Singer": ["sing high pitch notes, sing low pitch notes"],
-            "Guitarist": ["play jazz"]
-        }
+        "Rock": [
+            "play the drums with drumsticks",
+            "sing high pitch notes",
+            "play rock"
+        ],
+        'Metal': [
+            "play the drums with drumsticks",
+            "sing low pitch notes",
+            "play metal"
+        ],
+        "Jazz": [
+            "play the drums with drum brushes",
+            "sing high pitch notes",
+            "sing low pitch notes",
+            "play jazz"
+        ]
     }
 
     def __init__(self):
@@ -125,22 +143,31 @@ class ConcertTrackerApp:
         band.members.remove(musician)
         return f"{musician_name} was removed from {band_name}."
 
-    def is_band_ready(self, band: Band):
+    def __is_band_ready(self, band: Band):
         """checks if the band has at least 1 member of each musician type"""
         for kind in self.MUSICIANS.keys():
             if kind not in [type(member).__name__ for member in band.members]:
                 return False
         return True
 
-    def can_band_perform_genre(self, band: Band, genre: str):
-        skills_needed = self.SKILL_SET[genre]
-        for musician_type, skills in skills_needed.items():
-            for member in band.members:
-                if type(member).__name__ == musician_type:
-                    for skill in skills:
-                        if skill not in member.skills:
-                            return False
-        return True
+    # def __can_band_perform_genre(self, band: Band, genre: str):
+    #     required_skills = self.SKILL_SET[genre]
+    #     return all(skill == musician.skills for skill in required_skills for musician in band.members)
+
+    def __can_band_perform_genre(self, band: Band, genre: str):
+        required_skills = set(self.SKILL_SET[genre])
+        band_skills = set(skill for musician in band.members for skill in musician.skills)
+        return required_skills.issubset(band_skills)
+
+    # def __can_band_perform_genre(self, band: Band, genre: str):
+    #     skills_needed = self.SKILL_SET[genre]
+    #     for musician_type, skills in skills_needed.items():
+    #         for member in band.members:
+    #             if type(member).__name__ == musician_type:
+    #                 for skill in skills:
+    #                     if skill not in member.skills:
+    #                         return False
+    #     return True
 
     def start_concert(self, concert_place: str, band_name: str):
         """The method is to start a concert at the given place with the given band.
@@ -151,10 +178,10 @@ class ConcertTrackerApp:
         band = self.__get_band_by_name(band_name)
         concert = self.__get_concert_by_place(concert_place)
 
-        if not self.is_band_ready(band):
+        if not self.__is_band_ready(band):
             raise Exception(f"{band_name} can't start the concert because it doesn't have enough members!")
 
-        if not self.can_band_perform_genre(band, concert.genre):
+        if not self.__can_band_perform_genre(band, concert.genre):
             raise Exception(f"The {band_name} band is not ready to play at the concert!")
 
         profit = concert.audience * concert.ticket_price - concert.expenses
